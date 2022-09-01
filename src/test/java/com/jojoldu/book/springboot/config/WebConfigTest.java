c5ac1494db84389f267f981314854e0052c0cbd7
wonderWALL <wonderwall418@gmail.com>
2022-09-01 21:53:54 +0900

    WebConfigTest修正

--- src/test/java/com/jojoldu/book/springboot/config/WebConfigTest.java
+++ src/test/java/com/jojoldu/book/springboot/config/WebConfigTest.java
@@ -3,7 +3,6 @@ package com.jojoldu.book.springboot.config;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class WebConfigTest {
 
